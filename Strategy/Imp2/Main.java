400011fe6089664c2d8e5b81604c471d76547b93
Jay Chakra <jaychakra.jc@gmail.com>
2020-03-26 03:10:03 +0530

    Updated Main of IMP2

--- Strategy/Imp2/Main.java
+++ Strategy/Imp2/Main.java
@@ -3,6 +3,31 @@ package Strategy.Imp2;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Implementation 2");
+        MallardDuck md = new MallardDuck();
+        DecoyDuck dd = new DecoyDuck();
+        RubberDuck rd = new RubberDuck();
+        RedheadDuck rhd = new RedheadDuck();
+
+        // Design flaws: Since all of them are not coded to an interface hence a lot of code repetition
+        md.display();
+        md.quack();
+        md.swim();
+        md.fly();
+
+        dd.display();
+        dd.quack();
+        dd.swim();
+        dd.fly();
+
+        rd.display();
+//        rd.quack();
+        rd.swim();
+        rd.fly();
+
+        rhd.display();
+//        rhd.quack();
+        rhd.swim();
+//        rhd.fly();
+
     }
 }
